913a60f48bb9d7b97622bf719439c4d759307e59
odedbartov <odedoded777@gmail.com>
2019-02-14 17:17:09 +0200

    addquestion
    fixing and adjustment

--- DAL/dbRepository.js
+++ DAL/dbRepository.js
@@ -2,33 +2,30 @@ var sql = require("mssql");
 var config = require("./dbconfig");
 
 const dbPool = new sql.ConnectionPool(config, err => {
-    if (err) {
-        console.log(config);
-       // logger.log("error", "Can't create DB pool " + err + " stack:" + err.stack);
-        console.log(err)
-    }else{
-      console.log("connected to DB");
-    }
+  if (err) {
+    console.log(config);
+    // logger.log("error", "Can't create DB pool " + err + " stack:" + err.stack);
+    console.log(err);
+  } else {
+    console.log("connected to DB");
+  }
 });
 
-
 class DBContext {
-    executeInDB(callback) {
-        var req = dbPool.request();
-        req.input("FName", sql.NVarChar(50), "Jerry");
-        
+  executeInDB(callback) {
+    var req = dbPool.request();
+    req.input("FName", sql.NVarChar(50), "Jerry");
 
-        req.execute("GetUserByName", (err, data) => {
-            if (err) {
-                console.log("error", "Execution error calling 'getuserbyname'");
-            }
-            else {
-                callback(data.recordset);
-            }
-        });
-    } // executeInDB
+    req.execute("GetUserByName", (err, data) => {
+      if (err) {
+        console.log("error", "Execution error calling 'getuserbyname'");
+      } else {
+        callback(data.recordset);
+      }
+    });
+  } // executeInDB
 
-  addQuestion(question,callback){
+  addQuestion(question, callback) {
     var dbreq = dbPool.request();
     var answers = question.answers;
     dbreq.input("type", sql.NVarChar(50), question.type);
@@ -38,7 +35,7 @@ class DBContext {
     dbreq.input("tags", sql.NVarChar(300), question.tags);
     dbreq.execute("sp_addQuestion", (err, data) => {
       if (err) {
-        callback (err);
+        callback(err);
       } else {
         answers.forEach(element => {
           dbreq = dbPool.request();
@@ -47,48 +44,69 @@ class DBContext {
           dbreq.input("questionID", sql.Int, data.recordset[0].id);
           dbreq.execute("sp_addAnswer", (err, data) => {
             if (err) {
-              callback (err);
+              callback(err);
             }
           });
         });
+        callback(data.recordset);
+      }
+    });
+  }
+
+  addTest(test, callback) {
+    var dbreq = dbPool.request();
+    dbreq.input("name", sql.NVarChar(50), test.name);
+    dbreq.input("lastModifiedDate", sql.Date, test.lastModifiedDate);
+    dbreq.input("isActive", sql.Bit, test.isActive);
+    dbreq.input("version", sql.Int, test.version);
+    dbreq.input("link", sql.NVarChar(100), test.link);
+    dbreq.input("passingGrade", sql.Float, test.passingGrade);
+    dbreq.input("header", sql.NVarChar(100), test.header);
+    dbreq.input("messageOnSuccess", sql.NVarChar(100), test.messageOnSuccess);
+    dbreq.input("messageOnFailure", sql.NVarChar(100), test.messageOnFailure);
+    dbreq.input("mailSender", sql.NVarChar(100), test.mailSender);
+    dbreq.input("CC", sql.NVarChar(100), test.CC);
+    dbreq.input("BCC", sql.NVarChar(100), test.BCC);
+    dbreq.input(
+      "successMessageBody",
+      sql.NVarChar(400),
+      test.successMessageBody
+    );
+    dbreq.input(
+      "successMessageSubject",
+      sql.NVarChar(400),
+      test.successMessageSubject
+    );
+    dbreq.input("failMessageBody", sql.NVarChar(400), test.failMessageBody);
+    dbreq.input(
+      "failMessageSubject",
+      sql.NVarChar(400),
+      test.failMessageSubject
+    );
+    dbreq.input("fieldID", sql.Int, test.fieldID);
+
+    dbreq.execute("sp_addTest", (err, data) => {
+      if (err) {
+        callback(err);
+      } else {
         callback(data);
       }
     });
   }
 
-     addTest(test,callback){
-      var dbreq = dbPool.request();
-      dbreq.input("name", sql.NVarChar(50), test.name);
-      dbreq.input("lastModifiedDate", sql.Date,  test.lastModifiedDate );
-      dbreq.input("isActive", sql.Bit, test.isActive);
-      dbreq.input("version", sql.Int, test.version);
-      dbreq.input("link", sql.NVarChar(100), test.link);
-      dbreq.input("passingGrade", sql.Float, test.passingGrade);
-      dbreq.input("header", sql.NVarChar(100), test.header);
-      dbreq.input("messageOnSuccess", sql.NVarChar(100), test.messageOnSuccess);
-      dbreq.input("messageOnFailure", sql.NVarChar(100), test.messageOnFailure);
-      dbreq.input("mailSender", sql.NVarChar(100), test.mailSender);
-      dbreq.input("CC", sql.NVarChar(100), test.CC);
-      dbreq.input("BCC", sql.NVarChar(100), test.BCC);
-      dbreq.input("successMessageBody", sql.NVarChar(400), test.successMessageBody);
-      dbreq.input("successMessageSubject", sql.NVarChar(400), test.successMessageSubject);
-      dbreq.input("failMessageBody", sql.NVarChar(400), test.failMessageBody);
-      dbreq.input("failMessageSubject", sql.NVarChar(400), test.failMessageSubject);
-      dbreq.input("fieldID", sql.Int, test.fieldID);
-
-      dbreq.execute("sp_addTest",(err,data)=>{
-        if (err){
-          callback(err);
-        }else{
-          callback(data)
-        }
-      });
-
-     }
-
+  login(data, callback){
+    var dbreq = dbPool.request();
+    dbreq.input('email', sql.NVarChar(50), data.email)
+    dbreq.input('password', sql.NVarChar(50), data.password)
+    dbreq.execute("sp_login", (err, data) => {
+      if (err) {
+        callback(err);
+      }
+      else{
+        callback(data.recordsets);
+      }
+    })
+  }
 }
-
-
-
 
 module.exports = new DBContext();
--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,20 +1,26 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
-var mainDB = require('../DAL/dbRepository')
+var mainDB = require("../DAL/dbRepository");
 
-router.post('/temp', function (req, res, next) {
-//  res.json({ success: "great success" });
-
-  mainDB.executeInDB(data => res.json(data))
+router.post("/temp", function(req, res, next) {
+  //  res.json({ success: "great success" });
+  mainDB.executeInDB(data => res.json(data));
 });
 
 router.post("/addQuestion", function(req, res) {
-    mainDB.addQuestion(req.body,(data)=>{res.json(data)})
-   // mainDB.addQuestion(req.body).Subscribe(() => {res.end("questions created")});
+  mainDB.addQuestion(req.body, data => {
+    res.json(data);
+  });
+  // mainDB.addQuestion(req.body).Subscribe(() => {res.end("questions created")});
 
   //res.end("hello hello");
 });
 
+router.post("/login", function(req, res) {
+  mainDB.login(req.body, data => {
+    res.json(data);
+  })
+})
 
 module.exports = router;
