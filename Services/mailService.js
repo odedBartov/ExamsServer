e21ff6b0f945f22d835bfa65eeea49c71bbad9c8
Omer Carmeli <omercarmeli74@gmail.com>
2019-02-14 14:09:28 +0200

    activate mail

--- Services/mailService.js
+++ Services/mailService.js
@@ -2,15 +2,58 @@ var nodemailer = require('nodemailer');
 
 class mailer{
 
-     sendtestmail(){
-        
-        var transporter = nodemailer.createTransport({
+    transporter = nodemailer.createTransport({
         service: 'gmail',
         auth: {
             user: '@gmail.com',
             pass: ''
         }
     });
+
+    sendOnFinisedTest(source,target,subject,text){
+        //set Mail Options
+        var mailOptions = {
+            from: source,
+            to: target,
+            subject: subject,
+            text: text
+        };
+        transporter.sendMail(mailOptions, function(error, info){
+            if (error) {
+                console.log(error);
+            } else {
+                console.log('Email sent: ' + info.response);
+            }
+        });
+    }
+    sendActivateMail(source,target){
+        var mailOptions = {
+            from: source,
+            to: target,
+            subject: "welcome to exams system as admin",
+            text: "active your user in this link : www.link.com"
+        };
+        transporter.sendMail(mailOptions, function(error, info){
+            if (error) {
+                console.log(error);
+            } else {
+                console.log('Email sent: ' + info.response);
+            }
+        });
+    }
+
+
+
+
+     sendtestmail(){
+        
+    //     var transporter = nodemailer.createTransport({
+    //     service: 'gmail',
+    //     auth: {
+    //         user: '@gmail.com',
+    //         pass: ''
+    //     }
+    // });
   
     var mailOptions = {
         from: 'omercarmeli74@gmail.com',
